63bcea00b9cfea64c7fe717bb3b499ad41e0474c
Kin Shum <kinlshum@gmail.com>
2026-08-15 21:25:50 -0400

    Show JAV custom provider in Emby dashboard

--- JAV.Custom.Provider/Plugin.cs
+++ JAV.Custom.Provider/Plugin.cs
@@ -2,10 +2,11 @@ using JAV.Custom.Provider.Configuration;
 using MediaBrowser.Common;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Model.Drawing;
 
 namespace JAV.Custom.Provider;
 
-public sealed class Plugin : BasePluginSimpleUI<PluginConfiguration>
+public sealed class Plugin : BasePluginSimpleUI<PluginConfiguration>, IHasThumbImage
 {
     public const string ProviderName = "JAV_CUSTOM_PROVIDER";
     public const string ProviderId = "JAV_CUSTOM_PROVIDER";
@@ -20,4 +21,9 @@ public sealed class Plugin : BasePluginSimpleUI<PluginConfiguration>
     public override string Description => "Private actor metadata, aliases, URLs, images, and external IDs.";
     public override Guid Id => Guid.Parse("799a70be-0e93-45fc-8ec4-a4aac82e6471");
     public PluginConfiguration Configuration => GetOptions();
+
+    public Stream GetThumbImage() => new MemoryStream(Convert.FromBase64String(
+        "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAIAAAAlC+aJAAAAC0lEQVR4nO3BAQ0AAADCoPdPbQ43oAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAfg0QAAABf6nVAAAAAElFTkSuQmCC"));
+
+    public ImageFormat ThumbImageFormat => ImageFormat.Png;
 }
